9803c263056e5a3f611d2e8ca1fb5b19932b8737
FabianVillalobos4001 <favillalobos@alumnos.uai.cl>
2025-05-28 19:40:17 -0400

    P05-47 Se implementa funcion que notifica de manera insistente paquetes de acuerdo a su prioridad y tiempo en recepsion

--- BosserP05/server/util/prioridadPaquetes.ts
+++ BosserP05/server/util/prioridadPaquetes.ts
@@ -7,6 +7,7 @@ export function obtenerPaquetesPrioritarios(paquetes: Package[]): Package[] {
 
     const fechaRecepcion = new Date(pkg.fecha_recepcion);
     const horasEnBodega = (ahora.getTime() - fechaRecepcion.getTime()) / (1000 * 60 * 60);
+<<<<<<< HEAD:BosserP05/server/util/prioridadPaquetes.ts
     const minutosEnBodega =
       (ahora.getTime() - fechaRecepcion.getTime()) / (1000 * 60);
 
@@ -24,5 +25,19 @@ export function obtenerPaquetesPrioritarios(paquetes: Package[]): Package[] {
         
         //return horasEnBodega >= 24; // Otros tipos se consideran prioritarios si llevan más de 24 horas en recepción
     //}
+=======
+
+    switch (pkg.tipo) {
+      case "Urgente":
+        return horasEnBodega >= 2;
+      case "Congelado":
+        return horasEnBodega >= 0.5;
+      case "Frágil":
+        return horasEnBodega >= 6;
+      default:
+        
+        return horasEnBodega >= 24; // Otros tipos se consideran prioritarios si llevan más de 24 horas en recepción
+    }
+>>>>>>> b4f0bc6 (P05-47 Se implementa funcion que notifica de manera insistente paquetes de acuerdo a su prioridad y tiempo en recepsion):server/util/prioridadPaquetes.ts
   });
 }
