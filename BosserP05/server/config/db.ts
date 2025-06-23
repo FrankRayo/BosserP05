508d97e0e73d618e19f8afdc60d0315eaadf0969
FrankRayo <163183778+FrankRayo@users.noreply.github.com>
2025-06-23 19:42:06 -0400

    Test

--- BosserP05/server/config/db.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "../../deps.ts";
-
-const client = new MongoClient();
-await client.connect("mongodb://127.0.0.1:27017");
-
-const db = client.database("gestion_paquetes");
-
-export default db; // ✅ esto es fundamental
